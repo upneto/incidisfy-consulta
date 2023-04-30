e484d7307bcf7150912a53a833788e8fb35e761b
Ulisses Pereira da Silva Neto <upneto@gmail.com>
2023-04-30 15:13:33 -0300

    ajustes maven

--- incidisfy-backend-categoria/src/main/java/br/com/incidisfy/controller/HealthController.java
+++ incidisfy-backend-categoria/src/main/java/br/com/incidisfy/controller/HealthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/categoria")
 public class HealthController {
 
 	@GetMapping
